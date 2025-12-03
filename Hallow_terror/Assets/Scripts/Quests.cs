a9f30c31fd998e0db8c707f0d8942b39745724b6
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-12-03 00:10:15 -0300

    QuestTxt_melhorado

--- Hallow_terror/Assets/Scripts/Quests.cs
+++ Hallow_terror/Assets/Scripts/Quests.cs
@@ -9,16 +9,15 @@ public class Quests : MonoBehaviour
     public int indexQuest = 0;
     void Start()
     {       
-       queststxt = new string[8];
-       queststxt[0] = "Encontrar lanterna";
-       queststxt[1] = "Voltar para a vila";
-       queststxt[2] = "Volte o mais rápido possível para a vila!";
-       queststxt[3] = "Busque informações sobre os habitantes da vila";
-       queststxt[4] = "Fuja imediatamente da vila!";
-       queststxt[5] = "Verifique a casa isolada";
-       queststxt[6] = "Encontre a cura";
-       queststxt[7] = "Fuja do boss";
-       queststxt[8] = "Despeje a cura no rio";
+       queststxt = new string[7];
+       queststxt[0] = "Objetivo: Encontrar lanterna";
+       queststxt[1] = "Objetivo: Voltar para a vila";
+       queststxt[2] = "Objetivo: Volte o mais rápido possível para a vila!";
+       queststxt[3] = "Objetivo: Busque informações sobre os habitantes da vila";
+       queststxt[4] = "Objetivo: Fuja imediatamente da vila!";
+       queststxt[5] = "Objetivo: Verifique a casa isolada";
+       queststxt[6] = "Objetivo: Encontre a cura";
+       queststxt[7] = "Objetivo: Fuja do boss";     
 
        questAtual.text = queststxt[indexQuest];
     }
