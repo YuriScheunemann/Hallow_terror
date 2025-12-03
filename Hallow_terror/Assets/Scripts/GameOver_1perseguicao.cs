7a1795c3c87f08be54d7522c68db5a4b79998da5
YuriScheunemann <yuri.scheunemann@alunos.sc.senac.br>
2025-12-03 01:04:45 -0300

    MusicasAddictions&CreditsReformuled

--- Hallow_terror/Assets/Scripts/GameOver_1perseguicao.cs
+++ Hallow_terror/Assets/Scripts/GameOver_1perseguicao.cs
@@ -3,12 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class GameOver_1perseguicao : MonoBehaviour
 {
-   
+   public string Scene;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            SceneManager.LoadScene("JumpScary");
+            SceneManager.LoadScene(Scene);
         }
         
 
